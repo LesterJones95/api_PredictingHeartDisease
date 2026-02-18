a9d6938dbc20784de6277581f77102139a29e6bb
Lester Jones <nostresswithles@gmail.com>
2026-02-18 12:24:50 +0100

    finding on patient id works

--- src/app.py
+++ src/app.py
@@ -15,6 +15,19 @@ UPLOAD_FOLDER = './uploads'
 ALLOWED_EXTENSIONS = {'csv'} #, 'json'}
 TRAINED_MODEL = ydf.load_model('./final_model.keras')
 
+input_db_columns = ['id', 'age','sex','chest_pain_type','bp','cholesterol','fbs_over_120','ekg_results','max_hr','exercise_angina','st_depression','slope_of_st','number_of_vessels_fluro','thallium']    
+db_columns = ['id', 'age','sex','chest_pain_type','bp','cholesterol','fbs_over_120','ekg_results','max_hr','exercise_angina','st_depression','slope_of_st','number_of_vessels_fluro','thallium', 'prediction']    
+df_columns = ["id", "Age", "Sex", "Chest pain type", "BP", "Cholesterol", "FBS over 120", "EKG results", "Max HR", "Exercise angina", "ST depression", "Slope of ST", "Number of vessels fluro", "Thallium"]
+
+conn = psycopg2.connect(database="heartdisease_db",
+                    user='postgres', password='postgres', 
+                    host='127.0.0.1', port='5858'
+)
+conn.autocommit = True
+cursor = conn.cursor()
+#cursor.close()
+#conn.close()
+
 app = Flask(__name__)
 
 # Configure upload file path flask
@@ -45,9 +58,17 @@ def upload_file():
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             
             session['uploaded_data_file_path'] = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-            return render_template('index2.html')
+            
+            
+            csv_output_file = os.path.join(app.config['UPLOAD_FOLDER'], filename)
 
-        
+            with open(csv_output_file, 'r') as f:
+            # Notice that we don't need the csv module.
+                next(f) # Skip the header row.
+                cursor.copy_from(f, 'patient_details', columns=input_db_columns, sep=',')
+
+            return render_template('index2.html')  
+              
     return render_template("index.html")
 
 
@@ -64,37 +85,33 @@ def showPrediction():
     uploaded_df['Prediction'] = predictions
     # Converting to html Table
     uploaded_df_html = uploaded_df.to_html()
+
     return render_template('show_csv_data.html',
                            data_var=uploaded_df_html)
 
 
-@app.route('/show_db')
-@app.route('/show_db/<patiend_id>')
-def showDatabase(patiend_id=None):
-    conn = psycopg2.connect(database="heartdisease_db",
-                        user='postgres', password='postgres', 
-                        host='127.0.0.1', port='5858'
-)
-    conn.autocommit = True
-    cursor = conn.cursor()
-        
-    if not patiend_id:
+@app.route('/show_db', methods=['GET', 'POST'])
+@app.route('/show_db/<patient_id>', methods=['GET'])
+def showDatabase(patient_id=None):
+    if request.method == 'POST':
+        patient_id = request.form.get('patient_id')
+        print(f"Request method POST:\Patient ID:{patient_id}")
+
+    if not patient_id:
         cursor.execute("SELECT * FROM patient_details")
     else:
-        cursor.execute("SELECT * FROM patient_details WHERE id = (%s)", (patiend_id,))
+        cursor.execute("SELECT * FROM patient_details WHERE id = (%s)", (patient_id,))
 
     rows = cursor.fetchall()
-    print(f"Rows:\n{rows}")
-    db_columns = ['id', 'age','sex','chest_pain_type','bp','cholesterol','fbs_over_120','ekg_results','max_hr','exercise_angina','st_depression','slope_of_st','number_of_vessels_fluro','thallium', 'prediction']    
+    #print(f"Rows:\n{rows}")
+    
     df = pd.DataFrame(rows, columns=db_columns)
     #print(df)
     df = df.drop('prediction', axis=1)
     
-    df_columns = ["id", "Age", "Sex", "Chest pain type", "BP", "Cholesterol", "FBS over 120", "EKG results", "Max HR", "Exercise angina", "ST depression", "Slope of ST", "Number of vessels fluro", "Thallium"]
+    
     df = df.set_axis([df_columns], axis=1)
     print("------------------------------------------------------------")
-    print(df.columns)
-    print(type(df.columns[0]))
     df.columns = df.columns.get_level_values(0)
 
     print("Starting to predict ....\n")
@@ -102,11 +119,10 @@ def showDatabase(patiend_id=None):
     print("Finsihed....\n")
     df['Prediction'] = predictions
     
-    uploaded_df_html = df[['id', 'Prediction']].to_html(index=False)
+    uploaded_df_html = df[['id', 'Prediction']].drop_duplicates().to_html(index=False)
 
     
-    cursor.close()
-    conn.close()
+    
     return render_template('show_db_data.html',
                            data_var=uploaded_df_html)
 
